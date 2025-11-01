38ac2281f7b558377723d955b1e93ea6ec748956
colmnannacho <colmannacho7799@gmail.com>
2025-11-01 19:15:37 -0300

    agregamos al home la visualización de post, a medias

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useAuth } from "../context/AuthProvider"
 import type { Post, Usuario } from "../types/tipos"
 import { Button, Card, Col, Container, Row } from "react-bootstrap"
- 
+
 
 export default function Home() {
   const { usuario } = useAuth()
@@ -12,6 +12,7 @@ export default function Home() {
 
   useEffect(() => {
     document.title = "Anti-Social | Inicio"
+
   }, [])
 
   useEffect(() => {
@@ -36,29 +37,29 @@ export default function Home() {
 
   function nombreUsuario(userId: number | undefined): string {
     const usuarioEncontrado = usuarios.find((u) => u.id === userId)
+    console.log(posts[3].Post_Images?.[2].url)
     return usuarioEncontrado ? usuarioEncontrado.nickName : "Usuario inexistente"
   }
 
   return (
     <div>
       <Container className="my-3" >
+        <h2>Feed de Publicaciones</h2>
         <Row>
           {posts.map((post) => (
             <Col key={post.id} lg={12} md={12} sm={12} className="mb-4">
-              <div className="p-3 border bg-light">
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>{nombreUsuario(post.userId)}</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
-              </div>
-
+              <Card style={{ width: '100%'}}>
+                <Card.Body>
+                  <Card.Title>{nombreUsuario(post.userId)}</Card.Title>
+                  <Card.Text>
+                    {post.texto}
+                  </Card.Text>
+                  <Card.Img
+                    variant="top"
+                    src="https://picsum.photos/1920/1080"
+                    alt="Imagen del post" />
+                </Card.Body>
+              </Card>
             </Col>))}
         </Row>
       </Container>
